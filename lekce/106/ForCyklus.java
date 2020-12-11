d33cdd663943bb764e613d0b6b697bfdac2b1bea
Oren Holiš <oren.holis@bronet.cz>
2020-12-11 21:44:32 +0100

    přidání lekxe 106, Kostky a docs

---
+++ lekce/106/ForCyklus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lekce106;
+
+/**
+ *
+ * @author orenh
+ */
+public class ForCyklus {
+    public static void main(String[] args) {
+        int a = 10;
+        for(int i = 0; i < a; i++){
+            System.out.println(i);
+        }
+    }
+}
