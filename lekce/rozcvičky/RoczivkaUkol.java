364a32caf495305675537f435e52d8f33371e62d
Petr Chalupa <58788289+ChalupaPetr77@users.noreply.github.com>
2021-02-11 20:16:39 +0100

    Efektivita++

--- lekce/rozcvičky/RoczivkaUkol.java
+++ lekce/rozcvičky/RoczivkaUkol.java
@@ -11,10 +11,10 @@ package lekce109;
  */
 public class RoczivkaUkol {
     public static void main(String[] args) {
-        for (int a = 0; a < 100; a++) {
-            for(int b = 0; b < 100; b++){
-                for(int c = 0; c < 100; c++){
-                    if ((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) == 0.0 && a <= b && a > 0 && b > 0 && c > 0){
+        for (int a = 1; a < 100; a++) {
+            for(int b = 1; b < 100; b++){
+                for(int c = 1; c < 100; c++){
+                    if ((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) == 0.0 && a <= b){
                         System.out.println(a + " " + b + " " + c);
                     } 
                 }
